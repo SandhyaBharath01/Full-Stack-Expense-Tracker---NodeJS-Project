f4e0563651e81e0ef9b29427fd40bf5f37eed45a
SandhyaBharath01 <128684659+SandhyaBharath01@users.noreply.github.com>
2023-06-07 15:06:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const fs = require("fs");
 const cors = require("cors");
 app.use(cors());
 
-app.get("/",(req,res)=>{
-res.setHeader("Access-Control-Allow-Credentials","true");
-  res.send("API Running");
-});
 
 const dotenv = require("dotenv");
 dotenv.config();
